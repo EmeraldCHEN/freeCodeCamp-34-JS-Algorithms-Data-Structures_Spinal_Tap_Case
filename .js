c2b0893226897512e576709683d8d0886abf993e
EmeraldNZ <ahyuancy@gmail.com>
2018-12-26 20:23:32 +1300

    Update .js

--- .js
+++ .js
@@ -3,5 +3,23 @@ function spinalCase(str) {
 }
 
 spinalCase('This Is Spinal Tap');
-spinalCase("thisIsSpinalTap"); // return "this-is-spinal-tap".
-spinalCase("The_Andy_Griffith_Show"); // return "the-andy-griffith-show".
+spinalCase("thisIsSpinalTap"); // return "this-is-spinal-tap"
+spinalCase("The_Andy_Griffith_Show"); // return "the-andy-griffith-show"
+
+
+Split the string at one of the following conditions (converted to an array)
+
+a whitespace character [\s] is encountered
+underscore character [_] is encountered
+or is followed by an uppercase letter [(?=[A-Z])]
+Join the array using a hyphen (-)
+Lowercase the whole resulting string
+                               
+                               
+                               
+                               
+                               
+                               
+                               
+ // Retrieved from https://guide.freecodecamp.org/certifications/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/spinal-tap-case/                              
+                               
