275f99244bfdee899d75e00fafd32768ba0152c6
EmeraldNZ <ahyuancy@gmail.com>
2018-12-31 10:10:20 +1300

    Update .js

--- .js
+++ .js
@@ -1,12 +1,31 @@
+
+/*******************  Solution #1  **************************************************************************************/
+
 function spinalCase(str) {
    return str.split(/\s|_|(?=[A-Z])/).join('-').toLowerCase(); // Split str if a whitespace character [\s] or underscore character [_] is encountered
                                                                // or is followed by an uppercase letter [(?=[A-Z])]
                                                                // then join the array using a hyphen (-) & lowercase the whole resulting string
 }
-
 spinalCase('This Is Spinal Tap');
 spinalCase("thisIsSpinalTap"); // return "this-is-spinal-tap"
 spinalCase("The_Andy_Griffith_Show"); // return "the-andy-griffith-show"
+
+
+
+/*******************  Solution #2  **************************************************************************************/
+
+function spinalCase2(str) {
+    // Create a variable for the white space and underscores.
+    let regex = /\s+|_+/g;
+  
+    // Put a space before any encountered uppercase characters in str
+    str = str.replace(/([a-z])([A-Z])/g, '$1 $2');   
+  
+    // Replace space and underscore with -
+    return str.replace(regex, '-').toLowerCase();
+  }
+
+
 
 
 
